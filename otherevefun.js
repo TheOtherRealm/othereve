791fe3c40f2e5fec8b61f82110e3c29968a83b1e
Aaron E-J <the@otherrealm.org>
2018-11-24 16:27:57 -0500

    Created initial files

---
+++ otherevefun.js
@@ -0,0 +1,13 @@
+/*
+ Created		: Nov 24, 2018, 4:20:47 PM
+ Author		: Aaron E-J <the at otherrealm.org>
+ Copyright(C): 2018 Other Realm LLC
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the latest version of the GNU Affero General Public License as published by
+ the Free Software Foundation, using at least version 3.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details: <http://www.gnu.org/licenses/>.
+ */
